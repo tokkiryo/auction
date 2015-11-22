398b57c4a139670456005f844d1e33ef871e4dab
時崎 凌 <tokkiryo@gmail.com>
2015-11-22 11:41:00 +0900

    コントローラ開発(index)

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,4 +3,8 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
     #Itemというモデルに対してfindメソッドでidを検索させている
   end
+  def index
+    @items = Item.all
+  end
+  end
 end
