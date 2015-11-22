9aa9ec08462b4c48164432b9ffdd4a87d2520b67
時崎 凌 <tokkiryo@gmail.com>
2015-11-22 12:43:26 +0900

    コントローラ開発(create)

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -9,4 +9,23 @@ class ItemsController < ApplicationController
   def new
     @item = Item.new
   end
+def create
+    @item = Item.new(item_params)
+    # 上の行はインスタンスを作っただけ。saveメソッドで保存しないと、消える。
+    @item.save
+    redirect_to "/items/#{@item.id}"
+  end
+
+  private
+
+  # params.require(key).permit(filter)
+  # key
+  #   Strong Parameters を適用したい params の key を指定する。
+  # filter
+  #   Strong Parameters で許可するカラムを指定する。
+
+
+  def item_params
+    params.require(:item).permit(:name, :price, :seller_id, :description, :email, :image_url)
+  end
 end
